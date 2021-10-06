84345e53a3e8cc6748a09b87f86c5eadc346f916
JakeWMueller <jake.william.mueller@gmail.com>
2021-10-06 14:03:34 -0500

    Now in Development Environment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
-const path = require("path");
+// const path = require("path");
 
 dotenv.config();
 
@@ -21,14 +21,14 @@ mongoose
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/api/mappinapp/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/api/mappinapp/build')));
 
-  app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/api/mappinapp/build', 'index.html'));
-  });
-};
+//   app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '/api/mappinapp/build', 'index.html'));
+//   });
+// };
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(8800, () => {   //process.env.PORT || 5000, for production
   console.log("Backend server is running!");
 });
